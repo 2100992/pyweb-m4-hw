9a1161fc93e6630b9c3951becf73240e6be9e980
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-05-15 15:05:19 +0300

    release 1.0

--- helpers.js
+++ helpers.js
@@ -5,12 +5,18 @@ advice_url = "http://localhost:8080/api/forecasts"
 advice_url = "https://api.adviceslip.com/advice"
 */
 
+var dt = new Date();
+var time = dt.getFullYear() + "-" + dt.getMonth() + "-" + dt.getDay();
+d = $("#dt")
+d.html(time)
+
 $("#main-header").click(function() {
     console.log("1: внутри анонимной функции");
     $.getJSON(advice_url, function(data){
         console.log('Вот, что пришло:');
         advice = data["prophecies"];
-        set_secret_message(advice)
+        set_content_in_divs(advice)
+        /*set_secret_message(advice)*/
     })
     console.log("2: после взова внутри анонимной функции");
 })
